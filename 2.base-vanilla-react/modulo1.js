2a58f46ac2ae42283d8b8c7dba2b3d69f5dcf645
Carlos Marin Diaz <carlosmarind@gmail.com>
2025-11-10 20:17:21 -0300

    commit inicial

---
+++ 2.base-vanilla-react/modulo1.js
@@ -0,0 +1,7 @@
+function saludar(nombre) {
+    return `Saludos desde el modulo1 a ${nombre}`
+}
+
+let usuario = "Juan";
+
+console.log(saludar(usuario))
---
+++ 3.base-jsx/modulo2.js
@@ -0,0 +1,8 @@
+
+function saludar(nombre) {
+    return `Saludos desde el modulo2 a ${nombre}`
+}
+
+let usuario = "Ana";
+
+console.log(saludar(usuario))
